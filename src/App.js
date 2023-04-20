a38d7d2be8803f8c3110c08dfedbfcc254629dbc
Graeme McNish <graememcnish@gmail.com>
2023-04-19 19:40:35 -0700

    progress with map

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React,{ useEffect, useState } from 'react';
+import Character from './components/Character';
 
-import Character from "./components/Character"
+
 
 const App = () => {
-  const [data, setData] = useState();
-    useEffect(() => {
-      axios.get('https://swapi.dev/api/people/')
-        .then(res => {
-          console.log(res)
-        })
-        .catch(err => console.error(err))
-    }, [])
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+const [character, setCharacter] = useState([])
+useEffect(() => {
+  axios.get('https://swapi.dev/api/people/')
+  .then(res => {
+    setCharacter(res.data);
+  })
+})
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-    </div>
+        <Character character={character}/>
+      </div>
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,11 +1,10 @@
 import React from "react";
 // Write your Character component here
 
-const Character = (props) => {
-    
+const Character = ({character}) => {
+  return (
+  character.map((item) => <div>{item.name}</div>)
+  )
 }
 
-
-
-
-export default CharacterData;
+export default Character;
